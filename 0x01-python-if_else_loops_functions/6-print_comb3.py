958d8a9ce21c79e2832f0727f0c308e5c8424444
Joshua <joshuaogunlade1@gmail.com>
2022-11-29 14:05:02 -0800

    unique digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(1, 10):
+        if i >= j:
+            continue
+        elif i == 8 and j == 9:
+            print(f"{i}{j}")
+        else:
+            print(f"{i}{j}", end=", ")
