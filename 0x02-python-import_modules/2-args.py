3ed61d6b5317679aaebf003cfd93fa40bf63a4a9
Joshua <joshuaogunlade1@gmail.com>
2022-12-01 21:15:13 -0800

    making a script dynamic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 def print_arg(argv):
     n = len(argv) - 1
     if n == 0:
         print("{:d} argument.".format(n))
-        return
     else:
         if n == 1:
             print("{:d} argument:".format(n))
