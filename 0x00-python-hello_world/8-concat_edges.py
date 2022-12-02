5c7f5ed4c0d59fb1638aa31b10ae39d175578af6
Joshua <joshuaogunlade1@gmail.com>
2022-12-01 21:55:26 -0800

    using the print function

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
+ language that combines remarkable power with very clear syntax"
+str = str
 print(str)
