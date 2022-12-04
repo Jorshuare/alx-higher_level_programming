b1662d9e11c58221ee1e4cc37c8b90cd65f95e4d
Joshua <joshuaogunlade1@gmail.com>
2022-12-04 11:13:03 -0800

    a function that prints all integers of a list, in reverse order

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,6 @@ def print_arg(argv):
     n = len(argv) - 1
     if n == 0:
         print("{:d} argument.".format(n))
-        return
     else:
         if n == 1:
             print("{:d} argument:".format(n))
