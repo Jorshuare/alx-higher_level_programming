092ceb1eb32e3a13e4d03bf4a67870ee933b7d0c
Joshua <joshuaogunlade1@gmail.com>
2022-12-06 15:53:06 -0800

    modifies the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
     else:
-        if idx > len(my_list):
+        if idx > len(my_list) - 1:
             return my_list
         else:
             new = [i for i in my_list]
