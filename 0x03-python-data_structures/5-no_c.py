4c3d10445bc3e66a62f745f5e137097f4b8c837b
Joshua <joshuaogunlade1@gmail.com>
2022-12-10 02:08:39 -0800

    prints the input without C or c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 def no_c(my_string):
     for i in my_string:
         if i == "c" or i == "C":
-            my_string = my_string.replace(i, "")
-        return my_string
+            new_string = my_string.replace(i, "")
+            return new_string
