f8a448430fc00b96a69aaf2351a72317fd7502da
Joshua <joshuaogunlade1@gmail.com>
2022-12-13 14:42:17 -0800

    prints highest key

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(my_dict):
+    if my_dict is None or my_dict == {}:
+        return None
+    highest = max(my_dict.values())
+    for key, value in my_dict.items():
+        if value is highest:
+            return key
