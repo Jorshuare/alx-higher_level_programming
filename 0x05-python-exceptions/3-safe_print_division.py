1c5023bd602d86c810beacf6837d3cadffc4fbfb
Joshua <joshuaogunlade1@gmail.com>
2022-12-20 02:49:08 -0800

    a function that divides twp integers and print the result

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        div = a / b
+        print("Inside result: {:.1f}".format(div))
+    except:
+        div = None
+        print("Inside result: {}".format(div))
+    finally:
+        return div
