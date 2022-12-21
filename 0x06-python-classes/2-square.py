881d34f38931185bb823acec680d3df6c2151f32
Joshua <joshuaogunlade1@gmail.com>
2022-12-21 03:43:04 -0800

    :2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,10 +4,8 @@
 
 class Square:
     """Represent a square"""
-
     def __init__(self, size=0):
         """Initialize a new square.
-
         Args:
             size (int): The size of the new square.
         """
