174558910148f68443927c3f9b95d79fba3bb462
Joshua <joshuaogunlade1@gmail.com>
2022-12-21 01:29:04 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Represent a square"""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialize a new square.
 
         Args:
@@ -15,4 +15,5 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
            raise ValueError("size must be >= 0")
-        self.__size = size
+       self.__size = size
+
