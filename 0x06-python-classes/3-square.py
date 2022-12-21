ce546469bdd477d76d766f47afa0cd8e715fe0eb
Joshua <joshuaogunlade1@gmail.com>
2022-12-21 03:41:13 -0800

    :3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,10 +4,8 @@
 
 class Square:
     """Represent a square."""
-    
     def __init__(self, size=0):
         """Initialize a new Square.
-        
         Args: size (int): The size of the square.
         """
         if not isinstance(size, int):
