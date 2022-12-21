6ee1798dc482f148f7787fc0e794427b5973367d
Joshua <joshuaogunlade1@gmail.com>
2022-12-21 15:55:02 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,8 +7,9 @@ class Square:
 
     def __init__(self, size):
         """Initialize a new square.
-
         Args:
             size (int): The size of the new square.
+        Returns:
+            None
         """
         self.__size = size
