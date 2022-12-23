2b1e4a33018e1bb52a512b54813bcbb73f69d35d
Joshua <joshuaogunlade1@gmail.com>
2022-12-23 05:42:53 -0800

    a class Square

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         div = a / b
         print("Inside result: {:.1f}".format(div))
-    except:
+    except ZeroDivisionError:
         div = None
         print("Inside result: {}".format(div))
     finally:
