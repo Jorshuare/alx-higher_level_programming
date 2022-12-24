b4636c5e45025937763f5467acdb8a8ae8fb4010
Joshua <joshuaogunlade1@gmail.com>
2022-12-24 06:09:04 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     while True:
         try:
             if index < x:
-                print(my_list[index])
+                print(my_list[index], end="")
                 index += 1
             else:
                 print()
